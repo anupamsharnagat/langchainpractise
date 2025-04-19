e10de7de9d721761d98561b05df21048b5f95cdc
Anupam <anupamsharnagat@gmail.com>
2025-04-19 10:36:59 +0200

    starting first commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-if __name__ == '__main__':
-    print('hello langchain')
+from dotenv import load_dotenv
+import os
 
-
+if __name__ == "__main__":
+    print("hello langchain")
+    load_dotenv()
+    print(os.environ['SME_API_KEY'])
